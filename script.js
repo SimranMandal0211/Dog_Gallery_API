10bab5a7bda1c2518efa1b8af90db863f1fd5129
Simran Mandal <simranmandal0211@gmail.com>
2023-06-23 14:16:03 +0530

    fetch dog breed name and add to select options

---
+++ script.js
@@ -0,0 +1,10 @@
+var breedImage = $("#breed-image");
+var dropdown = $("#dog-breeds");
+
+$.get("https://dog.ceo/api/breeds/list/all", function (data, status) {
+    let dogBreeds = data.message;
+    for (let breed in dogBreeds) {
+        dropdown.append('<option value="' + breed + '">' + breed + '</option>');
+    }
+
+});
